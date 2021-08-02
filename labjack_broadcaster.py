bf2719310170b78508222132440f2053b129d912
jack <jstevenson567@gmail.com>
2021-08-02 14:35:55 -0700

    Added threading to run multiple sensors simultaneously, fixed interval issues by creating discrete interval timers for all sensors

--- labjack_broadcaster.py
+++ labjack_broadcaster.py
@@ -1,5 +1,5 @@
 """
-LabJack UDP Broadcast Script - Object-Oriented V0.0
+LabJack UDP Broadcast Script - Object-Oriented V0.3
 
 Code by Jack Stevenson and Chris Romsos, referencing code from LabJack Corporation
 
@@ -13,16 +13,62 @@ For more information or to view the latest version of this script, visit https:/
 
 from labjack import ljm
 import datetime
-import os
-import sys
-import time
+# import os
+# import sys
+# import time
 import socket
+from threading import Thread
 
 
-# Custom error for use in upload issues
-class UploadError(TypeError):
-    pass
+# Sensor class which is added to LabJack object to gather data over UDP
+class Sensor:
+    def __init__(self, pin="AIN0", port="30325", rate=1000, name="sensor", number=0):
+        self._pin = pin
+        self._port = port
+        self._rate = rate
+        self._name = name
+        self._intervalHandle = 0
+        self._number = number
 
+    def set_port(self, port):
+        """ Sets port name (default is 30325). """
+        self._port = port
+
+    def get_port(self):
+        """ Returns port name (default is 30325). """
+        return self._port
+
+    def set_pin(self, pin):
+        """ Sets socket number (default is 30325). """
+        self._pin = pin
+
+    def get_pin(self):
+        """ Returns socket number (default is 30325). """
+        return self._pin
+
+    def set_frequency_ms(self, frequency):
+        """ Sets upload frequency in milliseconds (default is 1000 ms). """
+        self._rate = frequency
+
+    def set_frequency_hz(self, frequency):
+        """ Sets upload frequency in hertz (default is 1 Hz). """
+        self._rate = (1/frequency) * 1000
+
+    def get_rate(self):
+        """ Returns sampling rate (default is 1000 ms)."""
+        return self._rate
+
+    def get_name(self):
+        """ Returns sensor name. """
+        return self._name
+
+    def set_number(self, number):
+        """ Set number for interval tracking. """
+        self._number = number
+
+    def get_number(self):
+        """ Get number for use in interval. """
+        return self._number
 
 # LabJack class with custom methods
 class LabJack:
@@ -32,7 +78,9 @@ class LabJack:
 
         self._connection = connection
         self._serial_number = serial_number
-        self._names = ["AIN0"]
+        #self._pins = ["AIN0"]
+        self._sensors = []
+        self._num_sensors = 0
 
         print("Trying to find LabJack...")
 
@@ -48,12 +96,7 @@ class LabJack:
             Connection method can be set when initializing LabJack object - ex. LJ = LabJack(connection="USB")
             Default connection is Ethernet.
             """)
-
-        # UDP Socket - DATA OUT
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self._socket_number = 30325
+            exit()
 
         # Get the connection metadata
         info = ljm.getHandleInfo(self._handle)
@@ -61,83 +104,65 @@ class LabJack:
               "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
               (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-        # Setup and call eReadName to read from AIN1 on the LabJack.
-        self._name = "AIN1"
-        self._rate = 1000  # in ms
-        self._rateus = self._rate * 1000  # in us (microseconds)
+        # UDP Socket - DATA OUT
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-        # Set interval handle
-        self._intervalHandle = 0
+    def add(self, sensor):
+        """ Adds a sensor to LabJack object. """
+        self._sensors.append(sensor)
+        self._num_sensors += 1
+        sensor.set_number(self._num_sensors)
 
-    def set_ports(self, ports):
-        """ Sets which ports will be used for data output (default is AIN0). """
+    # Potentially problematic implementation - check functionality
+    def remove(self, name):
+        """ Removes a sensor from LabJack object. """
+        if self._sensors == []:
+            print("Sensor list blank - couldn't find a sensor to remove.")
+            return
+        for item in self._sensors:
+            if item.get_name() == name:
+                self._sensors.remove(item)
+                self._num_sensors -= 1
 
-        # Clear current list of ports
-        self._names.clear()
+    def clear(self):
+        """ Removes all sensors from LabJack object. """
+        self._sensors.clear()
+        self._num_sensors = 0
+        return
 
-        # Add new ports to list of ports
-        for port in ports:
-            self._names.append(port)
-
-    def set_frequency_ms(self, frequency):
-        """ Sets upload frequency in milliseconds (default is 1000 ms). """
-        self._rate = frequency
-
-    def set_frequency_hz(self, frequency):
-        """ Sets upload frequency in hertz (default is 1 Hz). """
-        self._rate = (1/frequency) * 1000
-
-    def set_socket_number(self, socket_number):
-        """ Sets socket number (default is 30325). """
-        self._socket_number = socket_number
-
-    def start_broadcast(self):
-        """ Begin broadcast over UDP. """
-
-        # Get the current time to build a time-stamp.
-        app_start_time = datetime.datetime.now()
-        start_time_str = app_start_time.isoformat(timespec='milliseconds')
-        time_str = app_start_time.isoformat(timespec='milliseconds')
-
-        print(time_str)
-
-        # Print some program-initialization information
-        print("The time is: %s" % start_time_str)
+    def sensor_broadcast(self, sensor):
+        """ Broadcast method for sensors for multithreading - don't call this explicitly. """
 
         # Prepare final variables for program execution
-        self._rateus = self._rate * 1000
-        ljm.startInterval(self._intervalHandle, self._rateus)
-        self._numSkippedIntervals = 0
+        rateus = sensor.get_rate() * 1000
+        ljm.startInterval(sensor.get_number(), rateus)
 
-        self._last_tick = ljm.getHostTick()
+        #numSkippedIntervals = 0
+        last_tick = ljm.getHostTick()
 
         while True:
 
             try:
 
-                duration = 0
-
-                self._numSkippedIntervals = ljm.waitForNextInterval(self._intervalHandle)
-                self._cur_tick = ljm.getHostTick()
-                duration = (self._cur_tick - self._last_tick)/1000
+                ljm.waitForNextInterval(sensor.get_number())
+                cur_tick = ljm.getHostTick()
+                duration = (cur_tick - last_tick) / 1000
                 cur_time = datetime.datetime.now()
                 cur_time_str = cur_time.isoformat(timespec='milliseconds')
 
-                print(self._rate)
+                result = ljm.eReadName(self._handle, sensor.get_pin())
 
-                for name in self._names:
-                    # Read AIN0
-                    result = ljm.eReadName(self._handle, name)
+                message = f"{cur_time_str}, {sensor.get_name()}, {sensor.get_rate()}, {duration}, {result}"
+                print(message)
 
-                    message = f"{cur_time_str}, {duration}, {result}"
-                    print(message)
-
-                    print("Sending UDP")
-                    self._sock.sendto(bytes(message, "utf-8"), ("255.255.255.255", self._socket_number))
-                    print("Sending Done")
+                print("Sending UDP")
+                self._sock.sendto(bytes(message, "utf-8"), ("255.255.255.255", int(sensor.get_port())))
+                print("Sending Done")
 
                 # Set lastTick equal to curTick
-                self._last_tick = self._cur_tick
+                last_tick = cur_tick
 
             except ljm.LJMError:
                 print("LabJack not found")
@@ -160,12 +185,38 @@ class LabJack:
 
         # Close fi
         # Close handles
-        ljm.cleanInterval(self._intervalHandle)
+        ljm.cleanInterval(0)
         ljm.close(self._handle)
 
-    def stop_upload(self):
+    def start_broadcast(self):
+        """ Begin broadcasting over UDP. """
+
+        # Get the current time to build a time-stamp.
+        app_start_time = datetime.datetime.now()
+        start_time_str = app_start_time.isoformat(timespec='milliseconds')
+        # time_str = app_start_time.isoformat(timespec='milliseconds')
+
+        # Print some program-initialization information
+        print("The time is: %s" % start_time_str)
+
+        threads = []
+        for sensor in self._sensors:
+
+            t = Thread(target=self.sensor_broadcast, args=(sensor, ))
+            threads.append(t)
+            t.start()
+
+        print(self._sensors)
+        print(threads)
+
+        for thread in threads:
+            thread.join()
+
+    def stop_broadcast(self):
 
         print("\nFinished!")
+
+        raise KeyboardInterrupt
 
         # Get the final time
         appEndTime = datetime.datetime.now()
@@ -174,7 +225,7 @@ class LabJack:
 
         # Close fi
         # Close handles
-        ljm.cleanInterval(self._intervalHandle)
+        ljm.cleanInterval(0)
         ljm.close(self._handle)
 
         return 0
@@ -182,4 +233,11 @@ class LabJack:
 
 if __name__ == '__main__':
     Jack = LabJack()
+    WaterSensor = Sensor("AIN0", name="WaterSensor", rate=500)
+    MainSensor = Sensor("AIN1", name="MainSensor", rate=1000)
+    Jack.add(WaterSensor)
+    Jack.add(MainSensor)
+
+    print(Jack._sensors)
+
     Jack.start_broadcast()
