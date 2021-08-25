dfb42f421894ff0c485ea47d441eddf2cf781954
jack <jstevenson567@gmail.com>
2021-08-25 12:21:31 -0700

    Added some comments

--- labjack_receiver.py
+++ labjack_receiver.py
@@ -1,49 +1,34 @@
+"""
+LabJack UDP Receiver Script
+Object-Oriented V1.3 - Added threading support for multiple sensor output
+
+Code by Jack Stevenson and Chris Romsos
+
+This is a simple UDP receiving script to display data being returned from the LabJack. It does not save the data, so
+you have to add a data collection scheme appropriate for your application, such as saving to a CSV file.
+
+For more information or to view the latest version of this script, visit https://github.com/cromsos/CORIOLIX_labjack
+"""
 import socket
 
-port_a = 30325
-port_b = 30326
-port_c = 30327
-port_d = 30328
-port_e = 30329
+# Change to whatever port you are using for broadcast
+port = 30325
 
+# Create receiving socket
 a = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-a.bind(("", port_a))
+a.bind(("", port))
 
-b = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-b.bind(("", port_b))
+# Print message if connected to port but not receiving data yet (LabJack is not transmitting)
+print("waiting on port:", port)
 
-c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-c.bind(("", port_c))
-
-d = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-d.bind(("", port_d))
-
-e = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-e.bind(("", port_e))
-
-print("waiting on port:", port_a)
-print("waiting on port:", port_b)
-print("waiting on port:", port_c)
-print("waiting on port:", port_d)
-print("waiting on port:", port_e)
-
+# Loop to keep collecting data
 while True:
 
 	try:
 		data, addr = a.recvfrom(1024)
 		print(data)
 
-		# data, addr = b.recvfrom(1024)
-		# print(data)
-		#
-		# data, addr = c.recvfrom(1024)
-		# print(data)
-		#
-		# data, addr = d.recvfrom(1024)
-		# print(data)
-		#
-		# data, addr = e.recvfrom(1024)
-		# print(data)
+		# Add your preferred data collection scheme here
 
 	except KeyboardInterrupt:
 		print("UDP receive ended")
